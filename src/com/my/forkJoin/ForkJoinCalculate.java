19e3d739af144dc50edf1f34bbf3a63015281ab6
chensai <chen_sai_@126.com>
2018-11-11 09:21:40 +0800

    commit

---
+++ src/com/my/forkJoin/ForkJoinCalculate.java
@@ -0,0 +1,36 @@
+package com.my.forkJoin;
+
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinCalculate extends RecursiveTask<Long> {
+
+    private long[] numbers;
+    private int start;
+    private int end;
+    private final int THRESHOLD = 1000;
+
+    public ForkJoinCalculate(long[] numbers,int start,int end){
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+    }
+    @Override
+    protected Long compute() {
+        int length = end-start;
+        if(length<THRESHOLD){
+            return computeSequce();
+        }
+        ForkJoinCalculate left = new ForkJoinCalculate(numbers,start,length/2);
+        left.fork();
+        ForkJoinCalculate right = new ForkJoinCalculate(numbers,start+length/2,end);
+        long r = right.compute();
+        long l = left.join();
+        return r+l;
+    }
+
+    private long computeSequce() {
+        long result=0;
+        for(int i=start;i<end;i++) result+=numbers[i];
+        return result;
+    }
+}
