a186fb3c57328ad9b4356011b96b6fa16d5e7e33
chensai <chensai123>
2018-11-15 15:31:29 +0800

    commit

--- src/com/my/forkJoin/ForkJoinCalculate.java
+++ src/com/my/forkJoin/ForkJoinCalculate.java
@@ -1,6 +1,10 @@
 package com.my.forkJoin;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.LongStream;
 
 public class ForkJoinCalculate extends RecursiveTask<Long> {
 
@@ -32,5 +36,20 @@ public class ForkJoinCalculate extends RecursiveTask<Long> {
         long result=0;
         for(int i=start;i<end;i++) result+=numbers[i];
         return result;
+    }
+
+    public static void main(String[] args) {
+        long[] a = LongStream.rangeClosed(1,100000000).toArray();
+        ForkJoinCalculate forkJoinCalculate = new ForkJoinCalculate(a,0,a.length);
+        ForkJoinTask r = ForkJoinPool.commonPool().submit(forkJoinCalculate);
+       // long r  =   ForkJoinPool.commonPool().invoke(forkJoinCalculate);
+        try {
+            System.out.println(r.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
     }
 }
