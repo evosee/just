5ff51b8c0d9ffbd91817a5ab94040f33d4adb84b
chensai <chensai123>
2018-11-16 10:18:36 +0800

    commit

---
+++ src/com/my/thread/ThreadJoin.java
@@ -0,0 +1,26 @@
+package com.my.thread;
+
+public class ThreadJoin {
+    public static void main(String[] args) {
+        Thread a = new Thread(()->{
+            try {
+                Thread.sleep(3000);
+                System.out.println("a");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread b  = new Thread(()-> {
+            try {
+                a.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("b");
+        });
+        a.start();
+        b.start();
+
+
+    }
+}
