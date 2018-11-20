38d67349977ad9252a04d4d4e866159d1272aee7
chensai <chensai123>
2018-11-20 09:26:29 +0800

    commit

--- src/com/my/lang/Man.java
+++ src/com/my/lang/Man.java
@@ -1,7 +1,39 @@
 package com.my.lang;
 
-public class Man extends Person {
+
+import jdk.nashorn.internal.runtime.logging.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Man<T> extends Person {
     @TestAn
     private String addr;
     Person p;
+    List<String> lists;
+
+    public <T> T getLists(@Logger List<String> e,@Logger Object o) {
+        List<String> lists = new ArrayList<>();
+        return (T) lists;
+    }
+
+    public void setLists(List<String> lists) {
+        this.lists = lists;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public Person getP() {
+        return p;
+    }
+
+    public void setP(Person p) {
+        this.p = p;
+    }
 }
--- src/com/my/lang/MyClass.java
+++ src/com/my/lang/MyClass.java
@@ -5,9 +5,12 @@ import jdk.nashorn.internal.runtime.logging.Logger;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.TypeVariable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class MyClass {
     public static void main(String[] args) {
@@ -17,6 +20,10 @@ public class MyClass {
         try {
           //  Class[] c = Man.class.getDeclaredClasses();
            Field field =  Man.class.getDeclaredField("addr");
+            Method method = Man.class.getMethod("getLists", List.class,Object.class);
+            method.setAccessible(true);
+            TypeVariable<Method>[] m =  method.getTypeParameters();
+            System.out.println(field.getDeclaringClass());
             TestAn c=  field.getAnnotation(TestAn.class);
             Logger logger = c.annotationType().getAnnotation(Logger.class);
             System.out.println(logger);
