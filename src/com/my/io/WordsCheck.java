dfb430c2e64b7a827340827d4ce5c385bbb1a61a
chensai <chensai123>
2019-01-18 16:32:06 +0800

    commit

---
+++ src/com/my/io/WordsCheck.java
@@ -0,0 +1,14 @@
+package com.my.io;
+
+/**
+ * @Author: chensai
+ * @Date: 2019/1/18 16:20
+ * @Version 1.0
+ */
+public class WordsCheck implements WordsProcess {
+
+    @Override
+    public void process(String msg) {
+        System.out.println(msg);
+    }
+}
---
+++ src/com/my/io/WordsProcess.java
@@ -0,0 +1,10 @@
+package com.my.io;
+
+/**
+ * @Author: chensai
+ * @Date: 2019/1/18 16:19
+ * @Version 1.0
+ */
+public interface WordsProcess {
+    void process(String msg);
+}
