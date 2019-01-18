dce3119868a57d0c71abd10f3b8819de52ed45e5
chensai <chensai123>
2019-01-18 16:32:18 +0800

    commit

--- src/com/my/io/Words.java
+++ src/com/my/io/Words.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ import java.util.stream.Stream;
  * @Version 1.0
  */
 public class Words {
+
     public static List<String> getPaths(String source) {
         try {
             // URI uri = new URI(source);
@@ -30,16 +32,36 @@ public class Words {
         return null;
     }
 
-    public static int getCount(List<String> paths) {
+    //责任链模式引入处理链
+    public static int getCount(List<String> paths, List<WordsProcess> processes) {
         AtomicInteger result = new AtomicInteger();
         paths.stream().forEach(e -> {
             try {
                 BufferedReader reader = new BufferedReader(new FileReader(e));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    String finalLine = line;
+                    processes.forEach(p1 -> p1.process(finalLine));
                     result.getAndAdd(line.length());
                 }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
+        return result.intValue();
+    }
+
+    public static int getCount2(List<String> paths) {
+        AtomicInteger result = new AtomicInteger();
+        paths.stream().forEach(e -> {
+            try {
+                Path path = Paths.get(e);
+                List<String> s = Files.lines(path).collect(Collectors.toList());
+                s.forEach(f -> {
+                    System.out.println(f);
+                    result.getAndAdd(f.length());
+                });
+
             } catch (Exception e1) {
                 e1.printStackTrace();
             }
@@ -50,8 +72,13 @@ public class Words {
     public static void main(String[] args) {
         // String source = "file:///D://workspace";
         String source = "D://workspace";
-
+        List<WordsProcess> processes = new ArrayList<>();
+        processes.add(new WordsCheck());
         List<String> lists = getPaths(source);
-        System.out.println(getCount(lists));
+        System.out.println(getCount(lists, processes));
+
+
     }
+
+
 }
